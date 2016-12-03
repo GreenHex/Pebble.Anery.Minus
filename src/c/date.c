db3bbefb5e620b06ef67fdb92f8df167821c7682
GreenHex <GreenHex@gmail.com>
2016-12-03 18:43:42 +0530

    Changed date font

--- src/c/date.c
+++ src/c/date.c
@@ -1,6 +1,8 @@
 //
 // Copyright (C) 2016, Vinodh Kumar M. <GreenHex@gmail.com>
 //
+// Font: https://fonts.google.com/specimen/Gabriela
+//
 
 #include <pebble.h>
 #include "date.h"
@@ -18,6 +20,8 @@ static void date_layer_update_proc( Layer *layer, GContext *ctx ) {
   graphics_fill_rect( ctx, grect_inset( bounds, GEdgeInsets( DATE_WINDOW_OUTLINE_THK ) ), 0, GCornersAll );  
 }
 
+#define ALTERNATE_FONT
+
 static void date_text_layer_update_proc( Layer *layer, GContext *ctx ) {
   GRect bounds = layer_get_bounds( layer );
   static char date_text[3] = "";
@@ -29,8 +33,16 @@ static void date_text_layer_update_proc( Layer *layer, GContext *ctx ) {
   graphics_context_set_text_color( ctx, text_colour );
   snprintf( date_text, sizeof( date_text ), "%d", tm_time.tm_mday );
   bounds.origin.y -= DATE_TXT_VERT_ADJ;
+  
+  #ifdef ALTERNATE_FONT
+  GFont font = fonts_load_custom_font( resource_get_handle( RESOURCE_ID_FONT_GABRIELA_REGULAR_22 ) );
+  graphics_draw_text( ctx, date_text, font, bounds,
+                     GTextOverflowModeTrailingEllipsis, GTextAlignmentCenter, NULL );
+  fonts_unload_custom_font( font );
+  #else
   graphics_draw_text( ctx, date_text, fonts_get_system_font( FONT_KEY_ROBOTO_CONDENSED_21 ), bounds,
                      GTextOverflowModeTrailingEllipsis, GTextAlignmentCenter, NULL );
+  #endif
 }
 
 void date_init( Layer *parent_layer ) {
